8992209d7fbd29664f5afd47e6c08a7733bcf345
Ari Fiorino <ari@Aris-Mac.local>
2014-12-07 16:00:09 -0800

    Initial Commit

---
+++ Valina/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Valina
+//
+//  Created by Ari Fiorino on 12/7/14.
+//  Copyright (c) 2014 Azul Engineering. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
